38d9bcc1273a9c20de293a42dc946507588315db
Lady Quintero <laqgraphic@hotmail.com>
2023-07-28 11:31:36 +0200

    Étape 3 : Ajoutez des bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -21,16 +21,8 @@ const slides = [
 /// Get the arrow elements
 const leftArrow = document.querySelector('.arrow_left');
 const rightArrow = document.querySelector('.arrow_right');
-const dotsContainer = document.querySelector('.dots');
 
-// Function to generate the dots dynamically
-function createDots(numberOfDots) {
-  for (let i = 0; i < numberOfDots; i++) {
-    const dot = document.createElement('div');
-    dot.classList.add('dot');
-    dotsContainer.appendChild(dot);
-  }
-}
+
 
 // Add event listeners to the arrows
 leftArrow.addEventListener('click', () => {
@@ -39,13 +31,6 @@ leftArrow.addEventListener('click', () => {
     // or use alert() for a pop-up message
     // alert('Left arrow clicked!');
 
-    // Add logic to switch the selected dot
-    const selectedDot = document.querySelector('.dot_selected');
-    if (selectedDot) {
-      const previousDot = selectedDot.previousElementSibling || dotsContainer.lastElementChild;
-      selectedDot.classList.remove('dot_selected');
-      previousDot.classList.add('dot_selected');
-    }
 });
 
 rightArrow.addEventListener('click', () => {
@@ -53,16 +38,5 @@ rightArrow.addEventListener('click', () => {
     alert('Right arrow clicked!');
     // or use alert() for a pop-up message
     // alert('Right arrow clicked!');
-
-    // Add logic to switch the selected dot
-    const selectedDot = document.querySelector('.dot_selected');
-    if (selectedDot) {
-      const nextDot = selectedDot.nextElementSibling || dotsContainer.firstElementChild;
-      selectedDot.classList.remove('dot_selected');
-      nextDot.classList.add('dot_selected');
-    }
 });
 
-// Call the createDots function to generate the dots dynamically
-const numberOfSlides = 4; // Set the number of slides to 4
-createDots(numberOfSlides);
